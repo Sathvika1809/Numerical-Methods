09491214ec16a56358c5a990f51b1ae4366f75db
Sathvika <f20240192@hyderabad.bits-pilani.ac.in>
2026-04-06 15:23:24 +0530

    ok

---
+++ meanfunction.m
@@ -0,0 +1,5 @@
+function [output] = meanfunction(x,y)
+%UNTITLED2 Summary of this function goes here
+%   Detailed
+output = (x+y)/2;
+end
---
+++ untitled.m
@@ -0,0 +1,24 @@
+%basics of a function
+%function [out1,out2,.....,outN] = functionName(in1,in2,....,inM)
+    %Code body here
+    %Optional help comment
+    
+
+  %end
+
+%For One opt 
+% Brackets around opts are optional function y = myfun(x)
+%Omit = and brackets for no Outputs function myfun(x1,x2)
+% Save it as myfun.m ->Call like result = myfun(5,10)
+%For Example
+%function result = adder(x,y,z)
+    %result = x+y+z
+%end
+%Run adder(1,2,3) to get 6
+% Anonymous functions sq = @(x) x.^2; sq(5) returns 25
+%Use .* for element wise operations
+% Matlab is 1 based eg A(1) not 0 based
+%
+
+
+  
